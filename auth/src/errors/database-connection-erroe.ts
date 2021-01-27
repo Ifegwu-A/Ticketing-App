03d3f8853e1f7f1eb774ded09266e607bdb3e969
Kyrolos <>
2021-01-27 07:02:10 +0200

    debugging

---
+++ auth/src/errors/database-connection-erroe.ts
@@ -0,0 +1,19 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to a database';
+  statusCode = 500;
+  constructor() {
+    super('Error connecting to a database');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
